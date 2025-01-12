d481621e052229dde4a4534c694573540aebdba4
BGuJ <borisgujuman@hotmail.com>
2025-01-12 12:19:23 +0100

    Meniu 2

--- static/script.js
+++ static/script.js
@@ -1,13 +1,16 @@
 function toggleMenu() {
     const sidebar = document.querySelector('.sidebar');
+    const separator = document.querySelector('.separator');
     const menuTexts = document.querySelectorAll('.menu-text');
     if (sidebar.style.width === '60px') {
         sidebar.style.width = '200px';
+        separator.style.left = '200px';
         menuTexts.forEach(text => {
             text.style.display = 'inline';
         });
     } else {
         sidebar.style.width = '60px';
+        separator.style.left = '60px';
         menuTexts.forEach(text => {
             text.style.display = 'none';
         });
